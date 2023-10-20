c1c198f29f47d4d945a626022602e12c9208845d
Anthony <an.lala@laposte.net>
2023-10-21 01:27:34 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+
+    //Déclarations
+    int chiffreADeviner = 0;
+    int delta = 100;
+    int chiffreProposer = delta;
+
+    //Génération d'un chiffre aléatoire
+    srand( time(NULL) );
+    chiffreADeviner = rand()%101;
+
+    //Tant que le chiffre n'est pas deviner
+    while (1){
+
+        //Proposition système
+        printf("Le système propose %d \n",chiffreProposer);
+
+        //Comparaisons
+        if(chiffreProposer == chiffreADeviner){
+            break;
+        }else if (chiffreProposer > chiffreADeviner) {
+            printf("C'est moins !\n");
+            chiffreProposer -= delta;
+        }else{
+            printf("C'est plus !\n");
+            chiffreProposer += delta;
+        }
+
+        //Dichotomie
+        delta /=2;
+
+        //Ajustement
+        if(delta == 0){
+            delta = 1;
+        }
+
+    }
+
+    //Résultat
+    printf("Félicitation, vous avez deviner le chiffre %d\n",chiffreADeviner);
+
+    return 0;
+}
